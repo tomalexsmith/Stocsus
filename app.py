7449f7b758f19af4053bc170a38b557adb291386
b7073715 <b7073715@newcastle.ac.uk>
2021-12-12 10:40:12 +0000

    add database configuration setup

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Use your own database for testing
+# Connecting to team database will be done towards the end on campus/VM
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://DB_USER:DB_PASS@localhost:3306/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
