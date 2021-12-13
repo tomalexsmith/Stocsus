36638fd273f24f7ee6d3c915df3d656c700a2d6e
ketan <G.S.Kandola1@newcastle.ac.uk>
2021-12-13 18:56:25 +0000

    login, logout and view profile

--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
 # IMPORTS
-from flask import Blueprint, render_template, flash, redirect, url_for, request, session
+from flask import Blueprint, render_template, flash, redirect, url_for, \
+    request, session
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.security import check_password_hash
 from users.forms import register_form, login_form
 from models import Users
 from app import db
 
-#CONFIG
+# CONFIG
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
+
 
 # VIEWS
 # view registration
@@ -37,7 +41,46 @@ def register():
     # if request method is GET or form not valid re-render signup page
     return render_template('register.html', form=form)
 
+
 # view user login
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
+    if not session.get('logins'):
+        session['logins'] = 0
+    elif session.get('logins') >= 3:
+        flash('Number of incorrect logins exceeded')
+
     form = login_form()
+
+    if form.validate_on_submit():
+        session['logins'] += 1
+        user = Users.query.filter_by(email=form.email.data).first()
+
+        if not user or not check_password_hash(user.password, form.password.data):
+            if session['logins'] == 3:
+                flash('Number of incorrect logins exceeded')
+            elif session['logins'] == 2:
+                flash('Please check your login details and try again. 1 login attempt remaining')
+            else:
+                flash('Please check your login details and try again. 2 login attempts remaining')
+            return render_template('login.html', form=form)
+
+        if current_user.role == 'admin':
+            return redirect(url_for('admin.admin'))
+        else:
+            return redirect(url_for('users.search'))
+
+    return render_template('login.html', form=form)
+
+
+@users_blueprint.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', email=current_user.email)
+
+
+@users_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
