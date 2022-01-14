6e518ca7423afed22353a56386c4035d5c2bd1c1
Lawrence <l.banda@newcastle.ac.uk>
2022-01-14 15:03:18 +0000

    Created fixtures for tests

--- tests/functional/test_users.py
+++ tests/functional/test_users.py
@@ -1,105 +1,76 @@
 from app import create_app
 
 
-def test_home_page():
+def test_home_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/" page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app()
-
-    with  flask_app.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200
+    response = test_client.get('/')
+    assert response.status_code == 200
 
 
-def test_home_page_post():
+def test_home_page_post(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/" page is posted to (POST)
     THEN check that a '405' status code is returned
     """
-    flask_app = create_app()
 
-    with  flask_app.test_client() as test_client:
-        response = test_client.post('/')
-        assert response.status_code == 405
+    response = test_client.post('/')
+    assert response.status_code == 405
 
 
-def test_register_page():
+def test_register_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/register" page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app()
 
-    with  flask_app.test_client() as test_client:
-        response = test_client.get('/register')
-        assert response.status_code == 200
+    response = test_client.get('/register')
+    assert response.status_code == 200
 
-
-# def test_register_page_post():
-#     """
-#     GIVEN a Flask application configured for testing
-#     WHEN the "/register" page is posted to (POST)
-#     THEN check that a '405' status code is returned
-#     """
-#     flask_app = create_app()
-#
-#     with  flask_app.test_client() as test_client:
-#         response = test_client.post('/register')
-#         assert response.status_code == 405
-
-
-def test_login_page():
+def test_login_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/login" page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app()
 
-    with  flask_app.test_client() as test_client:
-        response = test_client.get('/login')
-        assert response.status_code == 200
+    response = test_client.get('/login')
+    assert response.status_code == 200
 
 
-def test_dashboard_page():
+def test_dashboard_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/dashboard" page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app()
 
-    with  flask_app.test_client() as test_client:
-        response = test_client.get('/dashboard')
-        assert response.status_code == 200
+    response = test_client.get('/dashboard')
+    assert response.status_code == 200
 
 
-def test_admin_page():
+def test_admin_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/admin" page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app()
 
-    with  flask_app.test_client() as test_client:
-        response = test_client.get('/admin')
-        assert response.status_code == 200
+    response = test_client.get('/admin')
+    assert response.status_code == 200
 
 
-def test_search_page():
+def test_search_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the "/search" page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app()
 
-    with  flask_app.test_client() as test_client:
-        response = test_client.get('/search')
-        assert response.status_code == 200
+    response = test_client.get('/search')
+    assert response.status_code == 200
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,46 +1,47 @@
-from database.models import Users, Blacklist, Favourite, WatchList
+import os
+import tempfile
+import pytest
+from flask import url_for
+from flask_login import login_user
+from app import create_app
+from database.models import Users, Blacklist, Favourite, WatchList, init_db
 
 
-def test_new_user():
+
+def test_new_user(new_user):
     """
     GIVEN a user model
     WHEN a user is created
     THEN check the email, password, role and banned fields are defined correctly
     """
-    user = Users('stocsustest@testemail.com', '@Password1', 'user', False)
-    assert user.email == 'stocsustest@testemail.com'
-    assert user.password != '@Password1'
-    assert user.role == 'user'
-    assert user.banned == False
+    assert new_user.email == 'stocsustest@testemail.com'
+    assert new_user.password != '@Password1'
+    assert new_user.role == 'user'
+    assert new_user.banned == False
 
 
-def test_blacklist():
+def test_blacklist(new_blacklist):
     """
     GIVEN a blacklist model
     WHEN a blacklist is created
     THEN check the supplier_name field is defined correctly
     """
-    blacklist = Blacklist('Test Supplier')
-    assert blacklist.supplier_name == 'Test Supplier'
+    assert new_blacklist.supplier_name == 'Test Supplier'
 
 
-def test_favourite():
+def test_favourite(new_favourite):
     """
     GIVEN a favourite model
     WHEN a favourite model is created
     THEN check the supplier_name field is defined correctly
     """
-    favourite = Favourite('Test Supplier')
-    assert favourite.supplier_name == 'Test Supplier'
+    assert new_favourite.supplier_name == 'Test Supplier'
 
 
-def test_watchlist():
+def test_watchlist(new_watchlist):
     """
     GIVEN a watchlist model
     WHEN a watchlist model is created
     THEN check the supplier_name field is defined correctly
     """
-    watchlist = WatchList('Test Supplier')
-    assert watchlist.part_number == 'Test Supplier'
-
-
+    assert new_watchlist.part_number == 'Test Supplier'
