953f61503ac8af08180b057a481e2d0ebb893f34
Lawrence <l.banda@newcastle.ac.uk>
2022-01-14 12:58:01 +0000

    Changes to app.py to ensure create_app function works

--- app.py
+++ app.py
@@ -103,6 +103,22 @@ def requires_roles(*roles):
 
     return wrapper
 
+# LOGGING
+class SecurityFilter(logging.Filter):
+    def filter(self, record):
+        return "SECURITY" in record.getMessage()
+
+fh = logging.FileHandler('Stocsus.log', 'w')
+fh.setLevel(logging.WARNING)
+fh.addFilter(SecurityFilter())
+formatter = logging.Formatter('%(asctime)s : %(message)s',
+                              '%m/%d/%Y %I:%M:%S %p')
+fh.setFormatter(formatter)
+
+logger = logging.getLogger('')
+logger.propagate = False
+logger.addHandler(fh)
+
 
 if __name__ == '__main__':
     app = create_app()
